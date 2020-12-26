0fb47a17125c37227a2e20079008c0aca8e7d75a
willneedit <willneedit@nurfuerspam.de>
2020-12-26 13:17:56 +0100

    Initial Commit

---
+++ MeshUVAtlas.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using Sceelix.Core.Annotations;
+using Sceelix.Core.Attributes;
+using Sceelix.Core.IO;
+using Sceelix.Core.Parameters;
+using Sceelix.Core.Procedures;
+using Sceelix.Mathematics.Data;
+using Sceelix.Meshes.Data;
+using Sceelix.Paths.Data;
+
+namespace Sceelix.MyNewEngineLibrary
+{
+    /// <summary>
+    /// Remaps the UV coordinate space of the individual faces to a region on a target texture
+    /// Example: Texture is a charset in a grid pattern, to have the mesh display an individual letter.
+    /// </summary>
+    [Procedure("8034DF9C-9104-453D-B249-B06D9073AC30", Label = "Mesh UV Atlas")]
+    public class MeshToPathExampleProcedure : SystemProcedure
+    {
+        private readonly SingleInput<MeshEntity> _input = new SingleInput<MeshEntity>("Input");
+        private readonly Output<MeshEntity> _output = new Output<MeshEntity>("Output");
+
+        /// <summary>
+        /// Bounding box inside the target texture
+        /// </summary>
+        private readonly DoubleParameter _UMin = new DoubleParameter("U Minimum", 0) { MinValue = 0, MaxValue = 1 };
+        private readonly DoubleParameter _VMin = new DoubleParameter("V Minimum", 0) { MinValue = 0, MaxValue = 1 };
+        private readonly DoubleParameter _UMax = new DoubleParameter("U Maximum", 1) { MinValue = 0, MaxValue = 1 };
+        private readonly DoubleParameter _VMax = new DoubleParameter("V Maximum", 1) { MinValue = 0, MaxValue = 1 };
+
+        /// <summary>
+        /// Stretch the source UV parameters to (0,0)..(1,1) before remapping
+        /// </summary>
+        private readonly BoolParameter _stretch = new BoolParameter("Stretch Source", true);
+
+
+        protected override void Run()
+        {
+            MeshEntity meshEntity = _input.Read();
+
+            double srcUMin = 9999, srcVMin = 9999;
+            double srcUMax = -9999, srcVMax = -9999;
+
+            // Determine the boundaries or set them to (0,0)..(1,1) and treat the UV coordinates as a subset within.
+            if(_stretch.Value)
+            {
+                foreach (Face face in meshEntity)
+                {
+                    foreach (HalfVertex hv in face.HalfVertices)
+                    {
+                        if (hv.UV0.X < srcUMin) srcUMin = hv.UV0.X;
+                        if (hv.UV0.X > srcUMax) srcUMax = hv.UV0.X;
+                        if (hv.UV0.Y < srcVMin) srcVMin = hv.UV0.Y;
+                        if (hv.UV0.Y > srcVMax) srcVMax = hv.UV0.Y;
+                    }
+                }
+            }
+            else
+            {
+                srcUMin = 0;
+                srcVMin = 0;
+                srcUMax = 1;
+                srcVMax = 1;
+            }
+
+            // Calculate the extent within the target texture
+            if(srcUMax == srcUMin || srcVMax == srcVMin)
+            {
+                Logger.Log("Source UV coordinates do not expand themselves to a face, cannot proceed", Logging.LogType.Error);
+                return;
+            }
+
+            double dstUStretch = (_UMax.Value - _UMin.Value) / (srcUMax - srcUMin);
+            double dstVStretch = (_VMax.Value - _VMin.Value) / (srcVMax - srcVMin);
+
+            // And remap all the UV coordinates.
+            foreach (Face face in meshEntity)
+            {
+                foreach(HalfVertex hv in face.HalfVertices)
+                {
+                    hv.UV0 = new Vector2D(
+                        (float)((hv.UV0.X - srcUMin) * dstUStretch + _UMin.Value),
+                        (float)((hv.UV0.Y - srcVMin) * dstVStretch + _VMin.Value)
+                    );
+
+                }
+            }
+
+            //finally, return the newly create pathentity
+            _output.Write(meshEntity);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+These are some (okay, one at the moment) extensions I did for the Sceelix Procedural Geometry Generator, available for free on Steam.
+
+## Compilation
+
+* Link (or copy) the 'Bin' folder of your Sceelix installation (usu. in <path to Steam Library>\steamapps\common\Sceelix\Bin) into this folder, rename it to "Assemblies". Otherwise you can edit the Reference paths in the project file to point towards the correct location.
+* Compile the library using Visual Studio. Tested fine with VS2019 Community Edition.
+
+## Installation
+
+* Link (or copy) the resulting output (Sceelix.Extensions.dll and Sceelix.Extensions.xml) to <your home folder>\Documents\Sceelix\Plugins (the folder next to the default Sceelix project folder)
+* having done that, you should see additional nodes in Sceelix under "Other"
+
+## New nodes
+
+### Mesh UV Atlas
+
+This node takes the UV coordinates of the vertices of a given mesh and translates them to a given coordinate space.
+
+Whereas the node "Mesh UV" would always create UV coordinates within the range of (0,0)..(1,1), unless scaled, this node relocates them to a different coordinate space.
+
+This is particularly useful if you have a texture containing a set of different images and the face of the mesh should display only one of them rather than all of them. Most common example would be a texture containing a whole charset and picking out specific letters.
